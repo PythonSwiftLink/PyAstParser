51135ae338d1101510f1aa380786bb7d577df688
psychowaspx <psychowaspx@gmail.com>
2022-12-28 11:58:56 +0100

    Create PyAst_Name.swift

---
+++ Sources/PyAstParser/PyAst_Name.swift
@@ -0,0 +1,29 @@
+
+
+import Foundation
+import PythonSwiftCore
+
+public class PyAst_Name: PyAstObject {
+    
+    public let type: AstType = .Name
+    
+    var id: String
+    
+    public var name: String { id }
+    
+    public required init(_ v: PythonObject) {
+        
+        if v.isNone {
+            id = "void"
+        } else {
+            let _id = v.id
+            
+            
+            id = _id.isNone ? "None": String(_id)
+        }
+        //print(self)
+        ////v.print_dict()
+    }
+    
+    
+}
