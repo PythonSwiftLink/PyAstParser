ab957b804beec7a9b1e45ce47aea72fe7fa387ba
psychowaspx <psychowaspx@gmail.com>
2023-01-10 23:53:04 +0100

    Create PyAst_Tuple.swift

---
+++ Sources/PyAstParser/PyAst_Tuple.swift
@@ -0,0 +1,23 @@
+
+
+import Foundation
+import PythonSwiftCore
+
+public class PyAst_Tuple: PyAstObject {
+    
+    public let type: AstType = .Tuple
+    
+    public var elts: [PyAstObject]
+    
+    public var name: String { fatalError() }
+    
+    public required init(_ v: PythonObject) {
+        
+        elts = v.elts.map(handlePyAst)
+//        print(self)
+//        v.print_dict()
+//        //fatalError()
+    }
+    
+    
+}
